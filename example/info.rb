5e4f40f2390837af390f2edaa1e355ec47f09e1c
Adam Stomski <adam.stomski@gmail.com>
2015-11-27 13:53:27 +0100

    info

--- example/info.rb
+++ example/info.rb
@@ -13,17 +13,8 @@ puts client.info_cmd("logs")
 puts "----------------"
 puts client.info_cmd("bins")
 puts "----------------"
-
-# indexes = client.info_cmd("sindex")
-# indexes = indexes.gsub!(/sindex\s/, '').split(";")
-
-# indexes.each do |index|
-#   puts "==============================="
-#   stats = index.split(":")
-
-#   stats.each do |st|
-#     puts st
-#   end
-# end
-
 puts client.list_indexes
+puts "----------------"
+puts client.statistics
+puts "----------------"
+puts client.namespaces
--- lib/aerospike_c_ruby/client.rb
+++ lib/aerospike_c_ruby/client.rb
@@ -1,4 +1,11 @@
+#
+# ruby implementation methods for AerospikeC::Client
+# create there methods that does not require blazing speed
+#
 AerospikeC::Client.class_eval do
+  #
+  # list all indexes in server
+  #
   def list_indexes
     index_ary = []
     indexes = info_cmd("sindex")
@@ -23,5 +30,36 @@ AerospikeC::Client.class_eval do
     end
 
     index_ary
+  end
+
+  #
+  # list statistic for server
+  #
+  def statistics
+    stats_hash = {}
+    stats = info_cmd("statistics")
+
+    stats = stats.gsub!(/statistics\s/, '').gsub!(/\n/, '').split(";")
+
+    stats.each do |st|
+      type  = ""
+      value = nil
+
+      st.split("=").each_with_index do |val, i|
+        type = val if i == 0
+        value = val if i == 1
+      end
+
+      stats_hash[type] = value
+    end
+
+    stats_hash
+  end
+
+  #
+  # list all namespaces
+  #
+  def namespaces
+    info_cmd("namespaces").gsub(/namespaces\s/, '').split(";")
   end
 end
