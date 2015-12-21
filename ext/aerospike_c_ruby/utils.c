67d5eef0fc7e5222cd3b4c079eb1a8f61ab9b44b
Adam Stomski <adam.stomski@gmail.com>
2015-12-21 12:52:38 +0100

    cstr

--- ext/aerospike_c_ruby/utils.c
+++ ext/aerospike_c_ruby/utils.c
@@ -377,18 +377,11 @@ VALUE as_hashmap2hash(as_hashmap * map) {
 
     as_string * i = as_string_fromval(key);
 
-    name = rb_str_new2(i->value);
+    name = rb_str_new_cstr(i->value);
     RB_GC_GUARD(name);
 
     val = as_val2rb_val(value);
     RB_GC_GUARD(value);
-
-    // rb_raise(rb_eRuntimeError, "key: %s, val: %s", val_inspect(name), val_inspect(val));
-
-    log_debug("----------------------------");
-    log_debug(val_inspect(name));
-    log_debug(val_inspect(val));
-    log_debug(val_inspect(hash));
 
     rb_hash_aset(hash, name, val);
   }
