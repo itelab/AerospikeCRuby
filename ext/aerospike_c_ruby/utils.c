c44be118700c29d84894d7929ec57b7061a9c28e
Adam Stomski <adam.stomski@gmail.com>
2015-12-21 12:59:43 +0100

    symbol?

--- ext/aerospike_c_ruby/utils.c
+++ ext/aerospike_c_ruby/utils.c
@@ -377,7 +377,7 @@ VALUE as_hashmap2hash(as_hashmap * map) {
 
     as_string * i = as_string_fromval(key);
 
-    name = rb_utf8_str_new_cstr(i->value);
+    name = ID2SYM(rb_intern(i->value));
     RB_GC_GUARD(name);
 
     val = as_val2rb_val(value);
