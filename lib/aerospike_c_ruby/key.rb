5b2a82c18e7ac5e771a0972075b50e6066d5c70a
Adam Stomski <adam.stomski@gmail.com>
2016-06-07 09:01:59 +0200

    key from string

--- lib/aerospike_c_ruby/key.rb
+++ lib/aerospike_c_ruby/key.rb
@@ -15,4 +15,21 @@ AerospikeC::Key.class_eval do
   def self._load(args)
     new(*args.split(':'))
   end
+
+
+  #----------------------------------------------------------------------------
+  ##
+  ## Create new key from string
+  ##
+  ## @param str [String] string ns:set:key
+  ## @param options = {} [Hash]
+  ##   @option key [Symbol] - :int if key should be integer
+  ##
+  ## @return [AerospikeC::Key] new key object
+  ##
+  def self.from_string(str, options = {})
+    ns, set, k, = str.split(":")
+    k = k.to_i if options[:key] == :int
+    AerospikeC::Key.new(ns, set, k)
+  end
 end
